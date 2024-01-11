8c1fc02a35a563c77f4ae2152609ef5e22615224
DevWambugu <venusdata100@gmail.com>
2024-01-12 02:55:09 +0300

    use union to output a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''This function  takes a string k
+and an int OR float v as arguments
+and returns a tuple'''
+from typing import Union
+from typing import Tuple
+
+
+def to_kv(k: str, v: Union[float, str]) -> Tuple[str, float]:
+    '''Return a tuple
+    The first element of the tuple
+    is the string k. The second element
+    is the square of the int/float v
+    and should be annotated as a float'''
+    v: float = v
+    square: float = v * v
+    return (k, square)
