340e0224d997f38f01ada9e5e55a2b6c9d4703a4
DevWambugu <venusdata100@gmail.com>
2024-01-12 02:34:09 +0300

    sum mixed types using union

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''This function sums the elements of a list
+(integer and floats) and returns a float'''
+from typing import List
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    '''returns the sum as a flaot type'''
+    return sum(mxd_lst)
