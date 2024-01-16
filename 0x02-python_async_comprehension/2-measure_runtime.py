3581b44e51378d4a6b52c3b6b286897f7ccf4e36
DevWambugu <venusdata100@gmail.com>
2024-01-16 21:48:40 +0300

    change order of comments

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+'''Run 4 coroutines at a go'''
 
 import asyncio
 import time
-'''Run 4 coroutines at a go'''
+
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -10,6 +11,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime():
     '''utilizes async.gather to run 4 functions
     and return the time taken to finish'''
+
     start_time = asyncio.get_event_loop().time()
     await asyncio.gather(
         async_comprehension(),
