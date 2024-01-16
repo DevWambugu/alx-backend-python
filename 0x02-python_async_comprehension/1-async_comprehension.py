e457842ed8f3382602c0b72060deeb766041b76b
DevWambugu <venusdata100@gmail.com>
2024-01-16 21:32:02 +0300

    add documentation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-
+'''generates a list of random
+numbers'''
 import asyncio
 
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    '''This function will generate random numbers by calling
+    the async generator function'''
     random_numbers = [i async for i in async_generator()]
     return random_numbers
