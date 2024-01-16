c68b4a5677d5aa6485562594dc3ade5ef5edeb62
DevWambugu <venusdata100@gmail.com>
2024-01-16 21:45:26 +0300

    return the total time taken

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+'''Run 4 coroutines at a go'''
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    '''utilizes async.gather to run 4 functions
+    and return the time taken to finish'''
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        )
+    ending_time = asyncio.get_event_loop().time()
+    time_taken = ending_time - start_time
+    return time_taken
