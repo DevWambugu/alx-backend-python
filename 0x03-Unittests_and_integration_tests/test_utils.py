614c682f4c7bc170622a8ce7a0bf197fd6f9e5b5
DevWambugu <venusdata100@gmail.com>
2024-02-05 22:12:20 +0300

    check whether an error is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,6 +20,17 @@ class TestAccessNestedMap(unittest.TestCase):
         values'''
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ["a"], KeyError, "Key 'a' not found in nested map"),
+        ({"a": 1}, ["a", "b"], KeyError, "Key 'b' not found in nested map"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path,
+                                         expected_exception, expected_message):
+        '''This function tests whether the
+        correct error is raised'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
