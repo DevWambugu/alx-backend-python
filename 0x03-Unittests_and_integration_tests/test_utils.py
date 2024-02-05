c29dbb545f54b46b985ba0993933f050a8232022
DevWambugu <venusdata100@gmail.com>
2024-02-05 22:43:05 +0300

    test the json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,8 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +32,27 @@ class TestAccessNestedMap(unittest.TestCase):
         correct error is raised'''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''This class implements the TestGetJson.test_get_json
+    method to test that utils.get_json returns the expected result'''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        '''This function tests the get_json method'''
+        mock_json = Mock()
+        mock_json.return_value = test_payload
+        mock_response = Mock()
+        mock_response.json = mock_json
+        mock_requests_get.return_value = mock_response
+        result = get_json(test_url)
+        mock_requests_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
